6415376f8b840f358dacbc10a63b9d5a58c80484
Junayet Alam <muhammadjunayetmaruf@gmail.com>
2025-02-19 16:15:48 +0600

    blog validation, model and interface has created

---
+++ src/app/modules/blog/blog.validation.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+const createBlogValidation = z.object({
+    body: z.object({
+        title: z.string({ required_error: 'Title is required' }),
+        content: z.string({ required_error: 'Content is required' }),
+        author: z.string({ required_error: 'Author is required' }),
+        isPublished: z.boolean().default(true),
+    })
+})
+const updateBlogValidation = z.object({
+    body: z.object({
+        title: z.string().optional(),
+        content: z.string().optional(),
+        author: z.string().optional(),
+        isPublished: z.boolean().optional(),
+    })
+        .partial()
+        .refine((data) => Object.keys(data).length > 0, {
+            message: "At least one field must be provided for update",
+        }),
+})
+
+export const BlogValidations = {
+    createBlogValidation,
+    updateBlogValidation
+}
